db53e4137f128872ff7ed0efe389bfcd9e63d933
RomanOlsh <r.olshanskiy@gmail.com>
2015-08-25 11:13:25 +0300

    add new user

--- WebApplication13/WebApplication13/Controllers/HomeController.cs
+++ WebApplication13/WebApplication13/Controllers/HomeController.cs
@@ -6,11 +6,184 @@ using System.Web.Mvc;
 using WebApplication13.Models.ViewModels;
 using WebApplication13.Models.DataModels;
 using System.Globalization;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
 
 namespace WebApplication13.Controllers
 {
     public class HomeController : Controller
     {
+        //Captch:
+        const int captchlength = 6;
+       
+        Random t = new Random();
+
+        public void gnrtword()
+        {
+            string mila = "";
+            for (int i = 1; i <= captchlength; i++)
+            {
+                char x = (char)t.Next('a', 'z'+1); // 'a' -> 'z'(+1)
+                while (mila.IndexOf(x) >= 0)
+                    x = (char)t.Next(97, 123);
+                mila += x;
+            }
+            Session["Crntcapthca"]=mila;
+        }
+   
+        public FileStreamResult gnrtcaptcha()
+        {
+            
+            const int width = 140, height = 60;
+            Bitmap objBitmap = new Bitmap(width, height);
+            Graphics objGraphics = Graphics.FromImage(objBitmap);
+            Color[] tseva = { Color.Black, Color.Red, Color.Blue, Color.White, Color.Pink, Color.Green, Color.Silver, Color.Orange, Color.Yellow, Color.Brown, Color.Purple };
+            Dictionary<Color, bool> alreadychoosed = new Dictionary<Color, bool>();
+            foreach (Color x in tseva)
+                alreadychoosed[x] = false;
+            Color fromgrad = tseva[t.Next(tseva.Length)];
+            alreadychoosed[fromgrad] = true;
+            Color tillgrad = tseva[t.Next(tseva.Length)];
+            alreadychoosed[tillgrad] = true;
+            LinearGradientBrush myHorizontalGradient = new LinearGradientBrush(new Rectangle(0, 0, width, height), fromgrad, tillgrad, 30F, false);
+            objGraphics.FillRectangle(myHorizontalGradient, 0, 0, width, height);
+            int xoffset = 0;
+            for (int j = 0; j < captchlength; j++)
+            {
+                FontStyle[] fsvlus = { FontStyle.Bold, FontStyle.Italic, FontStyle.Strikeout, FontStyle.Underline };
+                string[] fonts = { "Rod", "Times", "Serif", "Ariel" };
+                //Font fontBanner = new Font("Rod", 12, FontStyle.Bold);
+                Font fontBanner = new Font(fonts[t.Next(4)], 14 + t.Next(30), fsvlus[t.Next(4)]);
+                StringFormat SF = new StringFormat();
+                SF.Alignment = StringAlignment.Center;
+                SF.LineAlignment = StringAlignment.Center;
+                string ot = Session["Crntcapthca"].ToString().Substring(j, 1);
+                Color otcolor;
+                do
+                {
+                    otcolor = tseva[t.Next(tseva.Length)];
+                } while (alreadychoosed[otcolor]);
+                alreadychoosed[otcolor] = true;
+                objGraphics.DrawString(ot, fontBanner, new SolidBrush(otcolor), new RectangleF(xoffset,0, 45, height - t.Next(10)), SF);
+                xoffset += (13 + t.Next(10));
+            }
+            MemoryStream memstrm = new MemoryStream();
+            objBitmap.Save(memstrm, ImageFormat.Jpeg);
+            objGraphics.Dispose();
+            objBitmap.Dispose();
+            memstrm.Position = 0;
+            return new FileStreamResult(memstrm, "image/jpeg");
+        }
+
+        //------------------------------------------------------------------------------------------//
+
+       
+        //Upload photo:
+        public ActionResult AddUser() 
+        {
+            gnrtword();
+            return View();
+        }
+
+        bool itssignatureok(HttpPostedFileBase x)
+        {
+            bool rslt = false;
+            byte[] bfr = new byte[16];
+            x.InputStream.Read(bfr, 0, bfr.Length);
+            //http://www.fileformat.info/format/jpeg/egff.htm
+            // jfif or exif
+            if ((bfr[6] == 'J' && bfr[7] == 'F' && bfr[8] == 'I' && bfr[9] == 'F') || (bfr[6] == 'E' && bfr[7] == 'x' && bfr[8] == 'i' && bfr[9] == 'f'))
+                rslt = true;
+
+            return rslt;
+        }
+
+        bool isvalidfile(HttpPostedFileBase x, string prfx = "")
+        {
+            bool rslt = false;
+            string flnm = "";
+            //total max  size for All files set in web config section httpruntim, prm: maxrequestlength-1
+            if (x.ContentLength > 0 && x.ContentLength < FileManipulations.fileMaxSize)
+            {
+                flnm = Path.GetFileName(x.FileName);
+                if (FileManipulations.isExist(x.FileName))
+                    Session["msg"] += string.Format(prfx + "file: {0} already Exist!", flnm);
+                else
+                    if (Path.GetExtension(flnm).ToLower() == ".jpg" && (x.ContentType == "image/jpeg" || x.ContentType == "image/pjpeg")) // not html5 browser
+                    {
+                        if (itssignatureok(x))
+                            rslt = true;
+                        else
+                            Session["msg"] += string.Format(prfx + "file: {0} cant be saved as its content is Not of picture!", flnm);
+                    }
+                    else
+                        Session["msg"] += string.Format(prfx + "file: {0} cant be save as its not picture file!", flnm);
+            }
+            else if (x.ContentLength == 0)
+                Session["msg"] += string.Format(prfx + "file not Selected or cant upload Zero length file! ", flnm);
+            else
+                Session["msg"] += string.Format(prfx + "file: {0} cant be uploaded due site single file size limit restriction!", flnm);
+            return rslt;
+
+        }
+
+        public ActionResult dltit(string filename)
+        {
+            string onlyfilename = Path.GetFileName(filename);
+            if (!FileManipulations.deleteFile(onlyfilename))
+                Session["msg"] = string.Format("the file: {0} cant be Delete!", onlyfilename);
+            else
+                Session["msg"] = string.Format("the file: {0} deleted Ok!", onlyfilename);
+            return RedirectToAction("Index");
+        }
+       
+        //------------------------------------------------------------------------------------------//
+
+        //Add new user method:
+        [HttpPost]
+        public ActionResult addNewUser(HttpPostedFileBase loginpicture, User user)
+        {
+            //Captcha Validation:
+            if (user.Capthca.ToLower() == Session["Crntcapthca"].ToString().ToLower())
+            {
+                ModelState.AddModelError("", "Captcha Validation Failuer");
+                gnrtword();
+                return View();
+            }
+
+
+            //Add User Logic:
+
+            //Add Picture Logic:
+            Session["msg"] = "";
+            try
+            {
+                if (loginpicture == null) // for non html5 browsers
+                    Session["msg"] = "Login  picture Empty!";
+
+                else if (isvalidfile(loginpicture, "Login picture "))
+
+                    if (FileManipulations.addFile(loginpicture))
+                        Session["msg"] += string.Format("Login picture {0} uploaded Ok!", Path.GetFileName(loginpicture.FileName));
+                    else
+                        Session["msg"] += string.Format("Login picture file: {0} cant be save due File system problem!", Path.GetFileName(loginpicture.FileName));
+            }
+
+            catch (Exception prblm)
+            {
+                Session["msg"] += prblm.Message;
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        
+        
+
+
+
         public ActionResult Index()
         {
             ViewBag.scrptstr = TempData["errmessage"];
---
+++ WebApplication13/WebApplication13/Models/DataModels/FileManipulations.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace WebApplication13.Models.DataModels
+{
+    public class FileManipulations
+    {
+        public static int fileMaxSize = 1024 * 850;
+
+        //Get folder path:
+        public static string getStoreagefolder()
+        {
+            return HttpContext.Current.Server.MapPath("~/Uploads") + "\\";
+        }
+
+        //Gett all existing images:
+        public static List<string> getAllImages()
+        {
+            List<string> listOfImages = null;
+            try
+            {
+                string[] vec = Directory.GetFiles(getStoreagefolder(), "*.jpg");
+                for (int i = 0; i < vec.Length; i++)
+                    vec[i] = vec[i].Remove(0, vec[i].IndexOf(@"\Uploads"));
+                listOfImages = vec.ToList<string>();
+            }
+            catch
+            {
+                listOfImages = null;
+            }
+            return listOfImages;
+        }
+
+        //Add new file:
+        public static bool addFile(HttpPostedFileBase fileToAdd)
+        {
+            bool result = false;
+            try
+            {
+                fileToAdd.SaveAs(getStoreagefolder() + Path.GetFileName(fileToAdd.FileName));
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+            return result;
+        }
+
+        //Check if file exists:
+        public static bool isExist(string originalflnm)
+        {
+            string fullfilename = getStoreagefolder() + Path.GetFileName(originalflnm);
+            return File.Exists(fullfilename);
+        }
+
+        //Delete given file:
+        public static bool deleteFile(string filename)
+        {
+            bool result = false;
+            try
+            {
+                File.Delete(getStoreagefolder() + filename);
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
